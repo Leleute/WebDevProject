a634b3fcb2c80ca69967c9f7cc65f071f291888d
L-HommeSage <antoniadis.pablo@gmail.com>
2019-05-03 10:29:10 +0200

    Update pagepanier.php

--- pagepanier.php
+++ pagepanier.php
@@ -24,7 +24,26 @@ src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></s
 			<img class="basket" src="basket.png" width="30" height="30">
 			<img class="ece" src="ece.jpg">
 			
-			 <a href="Choix.php" style="color: black; position : absolute; top: 20px; left: 1250px;">Mon compte</a>
+			<?php 
+			session_start();
+			if($_SESSION['utilisateur']==0 || $_SESSION['utilisateur']== null)
+			{?><a href="Choix.php" style="color: black; position : absolute; top: 20px; left: 1250px;">Mon compte</a><?php  }
+			else{
+			$database = "projectweb";
+            $db_handle = mysqli_connect('localhost', 'root', '');
+            $db_found = mysqli_select_db($db_handle, $database);
+            
+            $login = $_SESSION['utilisateur'];
+            if ($db_found)
+             {
+            $sql = "SELECT ID,Login FROM acheteur WHERE ID LIKE '%$login%'";
+            $result = mysqli_query($db_handle, $sql);
+           $data= mysqli_fetch_assoc($result);
+           ?>
+           <a href="decoAcheteur.php" style="color: black; position : absolute; top: 20px; left: 1250px;"><?php echo $data['Login']; ?></a>
+           <?php 
+mysqli_close($db_handle);
+}}?>
 			 <a href="pagepanier.php" style="color: black; position : absolute; top: 50px; left: 1250px;">Mon panier</a>
 		
 		
@@ -34,7 +53,7 @@ src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></s
 	<center>
 			
 		 <p style="background-color: white; color: #469533;  font-size: 200%;">Mon panier</p>
-			<a href= "interface-site.html"> 
+			<a href= "interface-site-co.php"> 
 				<img class="home" src="home.png">
 			</a>
 
@@ -63,10 +82,3 @@ src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></s
 
 </body>
 </html>
-
-
-
-
-
-
-5
